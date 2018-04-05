ef5e10c89275149caf0cc2116db47fc872a94609
Marcus Cochran <mcochran@westinghousehs.org>
2018-04-05 18:50:56 +0000

    work

--- js/script.js
+++ js/script.js
@@ -4,5 +4,28 @@
 
 $(document).ready(function(){
   
+  function giphyURLWithSearchTerm(searchTerm) {
+}
 
+function appendImageToBody(srcURL) {
+    $('#image').html('<img src=' + srcURL + '/>');
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q="+ searchTerm + "&api_key=dc6zaTOxFJmzC&rating=g",
+      method: "GET",
+      success: function(response) {
+        console.log(response);
+        var url = response.data[0].images.original.url;
+           $('#image').html('<img src=' + url + '/>');
+      },
+    }); 
+    
+    $("#search").click(function(){
+    var inputValue = $("#srch-term").val();
+    var secondValue = $("#two").val();
+    callGiphyAPIWithSearchTerm(inputValue, secondValue);
+});
+      callGiphyAPIWithSearchTerm("pig");
 });
